10ffea09ebdbda0c4b4285f8b5cf804d99cad4b0
AliDalla <alidalla9500@gmail.com>
2025-02-09 22:51:21 +0300

    update

--- config.py
+++
@@ -1,9 +0,0 @@
-"""
-Configuration file for the Telegram bot.
-
-This file contains API keys, bot tokens, and other global settings.
-"""
-
-TOKEN = "8148810093:AAGIsDDPbtHLbi7B5W3MpgN4aTKAKgu0gVI"
-ADMIN_IDS = ["@DallaAli95", "DallaAli95"]  # Replace with actual Telegram user IDs of admins
-DB_PATH = "database.db"
