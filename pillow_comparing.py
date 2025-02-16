9f07d80f71ca3a7a8f76ad737c9dadfa81b7f494
AliDalla <alidalla9500@gmail.com>
2025-02-16 03:55:01 +0300

    add pillow_comparing.py  to compare images

--- pillow_comparing.py
+++ pillow_comparing.py
@@ -1,8 +1,8 @@
 from PIL import Image
 
-     def compare_images(image1_path, image2_path):
-         image1 = Image.open(image1_path)
-         image2 = Image.open(image2_path)
-         return list(image1.getdata()) == list(image2.getdata())
+def compare_images(image1_path, image2_path):
+    image1 = Image.open(image1_path)
+    image2 = Image.open(image2_path)
+    return list(image1.getdata()) == list(image2.getdata())
 
-     print(compare_images("image1.jpg", "image2.jpg"))
+print(compare_images("image1.jpg", "image2.jpg"))
