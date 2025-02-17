15e8e09135bc3f6020e7f414894c3837db783d0b
AliDalla <alidalla9500@gmail.com>
2025-02-17 23:53:19 +0300

    update config.....safe

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ Configuration file for the Telegram bot.
 
 This file contains API keys, bot tokens, and other global settings.
 """
+import os
 
 TOKEN = "8148810093:AAGIsDDPbtHLbi7B5W3MpgN4aTKAKgu0gVI"
 ADMIN_IDS = [6106281772, 6106281772]  # Replace with actual Telegram user IDs of admins
 DB_PATH = "bot_base.db"
-import os
 
 DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/Test"
